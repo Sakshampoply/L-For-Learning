8b0a30bc296ce3a353356d29d57e794f27b2aa67
Saksham Poply <sakshampoply@gmail.com>
2023-10-21 13:12:07 +0530

    responsive nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,16 @@
-import Image from "next/image"
-import Link from "next/link"
-import { signIn, useSession } from "next-auth/react"
+import Image from "next/image";
+import Link from "next/link";
+import { signIn, useSession } from "next-auth/react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useRef, useState } from "react";
 
 export default function Navbar() {
+    const { data: session } = useSession();
+
+    let [isOpen, setisOpen] = useState(false);
+
     return <>
-        <nav className="sm:flex justify-between items-center bg-[#E1DDD3] rounded-lg px-6 m-2 hidden">
+        <nav className="md:flex justify-between items-center bg-[#E1DDD3] rounded-lg md:px-6 m-2 ">
             <Link href={"/"}>
                 <Image
                     src="/Logo.png"
@@ -15,25 +21,32 @@ export default function Navbar() {
                     priority
                 />
             </Link>
-            <div>
+
+            <div onClick={() => setisOpen(!isOpen)} className="absolute right-8 top-8 cursor-pointer md:hidden">
+                {
+                    isOpen ? <button><FaTimes /></button> : <button><FaBars /></button>
+                }
+            </div>
+
+            <div className={`md:flex md:static absolute bg-[#E1DDD3] w-11/12 md:w-auto ${isOpen ? "top-24" : "top-[-490px]"}`}>
                 <Link href={"/aboutus"}>
-                    <button className="border-2 border-solid border-black rounded-full p-1 mx-2">
+                    <button className="block border-2 border-solid border-black rounded-full p-1 mx-2 my-4 md:my-0">
                         ABOUT US
                     </button>
                 </Link>
                 <Link href={"/podcast"}>
-                    <button className="border-2 border-solid border-black rounded-full p-1 mx-2">
+                    <button className="block border-2 border-solid border-black rounded-full p-1 mx-2 my-4 md:my-0">
                         PODCAST
                     </button>
                 </Link>
                 <Link href={"/blog"}>
-                    <button className="border-2 border-solid border-black rounded-full p-1 mx-2">
+                    <button className="block border-2 border-solid border-black rounded-full p-1 mx-2 my-4 md:my-0">
                         BLOG
                     </button>
                 </Link>
             </div>
-            <div className="flex">
-                <div className="flex">
+            <div className={`block md:flex md:static absolute bg-[#E1DDD3] w-11/12 md:w-auto ${isOpen ? "top-[260px]" : "top-[-490px]"}`}>
+                <div className="flex my-2 md:my-0 mx-2 md:mx-0">
                     <Image
                         src={"/Barcode.png"}
                         width={150}
@@ -42,30 +55,12 @@ export default function Navbar() {
                 </div>
                 <div>
                     <Link href={"/profile"}>
-                        <button onClick={signIn} className="border-2 border-solid border-black rounded-full p-1 mx-2">
+                        <button onClick={signIn} className="block border-2 border-solid border-black rounded-full p-1 mx-2 my-4 md:my-0">
                             LOGIN IN
                         </button>
                     </Link>
                 </div>
             </div >
         </nav >
-
-        <nav className="sm:hidden bg-[#E1DDD3] rounded-lg">
-            <div className="flex justify-between">
-                <Link href={"/"}>
-                    <Image
-                        src="/Logo.webp"
-                        alt="Logo"
-                        className="p-4"
-                        width={80}
-                        height={24}
-                        priority
-                    />
-                </Link>
-                <div className="p-6">
-                    <h1>\/</h1>
-                </div>
-            </div>
-        </nav>
     </>
 }
