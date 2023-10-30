810fb3ae8d9d5ead8b47c3118d18fc043fd62f2d
Saksham Poply <sakshampoply@gmail.com>
2023-10-30 13:18:04 +0530

    hero changes

--- components/Hero.js
+++ components/Hero.js
@@ -23,7 +23,7 @@ export default function Hero() {
                 LEARNING
             </div>
             <div className="flex">
-                <div className="absolute left-0 top-[550px] sm:top-[500px] w-[140px]">
+                <div className="absolute left-0 top-[550px] sm:top-[500px] sm:w-[260px] w-[140px]">
                     <Image
                         src="/SoundCloud.png"
                         alt="SoundCloud Podcast"
@@ -33,7 +33,7 @@ export default function Hero() {
                         priority
                     />
                 </div>
-                <div className="absolute left-[130px] sm:left-[320px] top-[550px] sm:top-[500px] w-[120px]">
+                <div className="absolute left-[130px] sm:left-[320px] top-[550px] sm:top-[500px] sm:w-[225px] w-[120px]">
                     <Image
                         src="/Google.png"
                         alt="Google Podcast"
@@ -43,7 +43,7 @@ export default function Hero() {
                         priority
                     />
                 </div>
-                <div className="absolute left-[240px] sm:left-[610px] top-[550px] sm:top-[500px] w-[130px]">
+                <div className="absolute left-[240px] sm:left-[610px] top-[550px] md:top-[550px] sm:top-[500px] sm:w-[240px] w-[130px]">
                     <Image
                         src="/Castos.png"
                         alt="Castos Podcast"
@@ -53,7 +53,7 @@ export default function Hero() {
                         priority
                     />
                 </div>
-                <div className="absolute left-[60px] sm:left-[142px] top-[620px] w-[130px]">
+                <div className="absolute left-[60px] sm:left-[142px] top-[620px] sm:w-[225px] w-[130px]">
                     <Image
                         src="/Spotify.png"
                         alt="Spotify Podcast"
@@ -63,7 +63,7 @@ export default function Hero() {
                         priority
                     />
                 </div>
-                <div className="absolute left-[170px] sm:left-[440px] top-[610px] w-[110px]">
+                <div className="absolute left-[190px] sm:left-[440px] top-[610px] sm:w-[200px] w-[110px]">
                     <Image
                         src="/Apple.png"
                         alt="Apple Podcast"
@@ -73,7 +73,7 @@ export default function Hero() {
                         priority
                     />
                 </div>
-                <div className="absolute right-8 sm:right-12 top-[290px] w-[250px]">
+                <div className="absolute right-8 sm:right-12 top-[290px] sm:top-[270px] sm:w-[550px] w-[250px]">
                     <Image
                         src="/Smiley.png"
                         alt="Smiely Design-Element"
